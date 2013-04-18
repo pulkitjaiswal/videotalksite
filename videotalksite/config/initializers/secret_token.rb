3fef402d874171b78d3760242f92d19f094bd0e5
Pulkit Jaiswal <pulkitj@stanford.edu>
2013-04-18 16:54:44 -0700

    sqlite issue

--- videotalksite/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Videotalksite::Application.config.secret_token = 'f76c24391b4f75856c7a3a7391b01fcc9f990d79d35aa4c2db2faa8ebad5206d0ba9e0e1b2c41b0b56bfbdb9fa283215b0b43537de1f622eab07ba279178f113'
